8835e83df2577ace382108bac1bd65b35649f210
yeasin13 <yeasinarafat8001@gmail.com>
2023-01-31 21:08:08 +0600

    react-icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import {BsFacebook} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
 
-    <a href="https://www.linkedin.com/in/md-yasin-748a95262/" target="_blank"></a>
-    <a href="https://github.com/yeasin13" target="_blank"></a>
-    <a href="https://www.dribbble.com" target="_blank"></a>
+    <a href="https://www.linkedin.com/in/md-yasin-748a95262/" target="_blank"><BsLinkedin/></a>
+    <a href="https://github.com/yeasin13" target="_blank"><BsGithub/></a>
+    <a href="https://www.facebook.com/yeasin89/" target="_blank"><BsFacebook/></a>
     </div>
   )
 }

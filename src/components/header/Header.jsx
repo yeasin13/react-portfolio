f74e15302570b613aaffe3f24cd4ca22e8dbbf0a
yeasin13 <yeasinarafat8001@gmail.com>
2023-02-01 12:51:56 +0600

    header-section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/1 (8).jpg'
+import ME from '../../assets/20221222_225705-removebg-preview.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
